ad8d98dea487a81041dac9fab6600e7a6b3aafda
Adrian-rospx <adrianrosu5400@gmail.com>
2026-03-03 23:19:23 +0200

    feature: view toggle button

--- src/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-  return (
-    <header className="flex flex-row justify-center p-1 
-                    bg-clk-primary border-b-2 border-clk-accent">
-      <h1 className="text-2xl font-bold text-center">FTC Decode Launcher Simulation</h1>
-    </header>
-  );
-}
---
+++ src/components/ViewToggle.tsx
@@ -0,0 +1,29 @@
+import { useStore } from "@nanostores/react";
+import { Grid2x2 } from "lucide-react";
+import { ChartLine } from "lucide-react";
+
+import { $view } from "../stores/settings";
+import { useMemo } from "react";
+
+export default function ViewToggle() {
+  const view = useStore($view);
+
+  const {Icon, func} = useMemo(() => {
+    switch (view) {
+      case "side": return {
+        Icon: ChartLine, 
+        func: () => $view.set("field")
+      };
+      case "field": return {
+        Icon: Grid2x2, 
+        func: () => $view.set("side"),
+      };
+    } 
+  }, [view]);
+
+  return (
+    <button className="hidden not-md:block hover:cursor-pointer" onClick={func}>
+      <Icon />
+    </button>
+  )
+}
