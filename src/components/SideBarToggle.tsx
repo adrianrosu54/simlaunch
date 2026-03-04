e844d45c5e3c8f5a5fd9f7640497eaa932723e6e
Adrian-rospx <adrianrosu5400@gmail.com>
2026-03-04 02:17:14 +0200

    added: nanostore dynamic sidebar implementation

--- src/components/SideBarToggle.tsx
+++ src/components/SideBarToggle.tsx
@@ -1,23 +1,13 @@
 import { MenuIcon, X } from "lucide-react"
+import { $isSidebarOpen } from "../stores/settings";
 
 export default function SideBarToggle({type}: {type: "open" | "close"}) {
-  const sidebar =  document.getElementById("sidebar");
-  const contents = document.getElementById("content");
-  
   return (
     (type === "open") 
-      ? <button className="hover:cursor-pointer" onClick={() => {
-          sidebar?.classList.toggle("md:w-64", true);
-          sidebar?.classList.toggle("w-full", true);
-          sidebar?.classList.toggle("w-0", false);
-      }}>
+      ? <button className="hover:cursor-pointer" onClick={() => $isSidebarOpen.set(true)}>
         <MenuIcon />
       </button>
-      : <button className="hover:cursor-pointer" onClick={() => {
-          sidebar?.classList.toggle("md:w-64", false);
-          sidebar?.classList.toggle("w-full", false);
-          sidebar?.classList.toggle("w-0", true);
-      }}>
+      : <button className="hover:cursor-pointer" onClick={() => $isSidebarOpen.set(false)}>
         <X />
       </button>
   );
--- src/stores/settings.ts
+++ src/stores/settings.ts
@@ -2,3 +2,5 @@ import { atom } from "nanostores";
 
 export type View = "side" | "field";
 export const $view = atom<View>("field");
+
+export const $isSidebarOpen = atom(false);
