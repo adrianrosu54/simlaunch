57a54b0280581c0a6d2b70aa5e4b9705a7efbbb2
Adrian-rospx <adrianrosu5400@gmail.com>
2026-03-05 23:17:31 +0200

    release: second draft

---
+++ README.md
@@ -0,0 +1,15 @@
+# SimLaunch
+
+## FTC Decode Flywheel Launcher simulation
+
+Web visualiser application for robotic launcher physics in the
+FTC Decode Season made by team #19075 Clockworks.
+
+It features live interactive charts for parameters of the launcher,
+including a trajectory plot, field view display and impact error
+calculation.
+
+The calculations take into account gravity and basic drag force,
+along with the flywheel launch efficiency.
+
+Project made with Astro and React.
--- src/components/ImpactCard.tsx
+++ src/components/ImpactCard.tsx
@@ -11,14 +11,14 @@ export default function ImpactReading() {
     <div className="size-full flex flex-col justify-center">
       <div className="w-full flex flex-col text-center font-sans text-md md:text-lg font-semibold text-clk-text-secondary">
         <article className="flex flex-col p-2">
-          <span>Impact point ({unit})</span>
+          <span>Impact point</span>
           <span className="font-mono text-sm md:text-lg italic text-clk-text-primary">
-            ({toDisplay(impact.x, unit).toFixed(2)}, {toDisplay(impact.y, unit).toFixed(2)})
+            ({toDisplay(impact.x, unit).toFixed(2)}, {toDisplay(impact.y, unit).toFixed(2)}) {unit}
           </span>
         </article>
         <article className="flex flex-col p-2">
           <span>Error reading</span>
-          <span className="font-mono text-xl md:text-2xl italic text-clk-text-primary">
+          <span className="font-mono text-md md:text-xl italic text-clk-text-primary">
             {toDisplay(error, unit).toFixed(2)} {unit}
           </span>
         </article>
